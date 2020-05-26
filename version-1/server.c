9cceac8c402dd4a05bcc760ffb56dd19d0e9465c
Teymur Hasanzade <aghateymur.hasanzada@ufaz.az>
2020-05-26 19:32:07 +0400

    Fixed version 1

--- version-1/server.c
+++ version-1/server.c
@@ -1,67 +1,27 @@
-// server.c version 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<signal.h>
 
-// Include libraries
+char *sig_type[65];
 
-#include <stdio.h> 
-#include <sys/ipc.h> 
-#include <sys/msg.h> 
-#include <string.h> 
-#include <stdlib.h> 
-#include <signal.h> 
-#include <pthread.h>
+void signal_handler(int sig){
+    printf("This is %s type haiku\n", sig_type[sig]);
+}
 
-//Declare global variables
+int main(int argc, char const *argv[])
+{
+    sig_type[SIGINT] = "Japanese";
+    sig_type[SIGQUIT] = "Western";
+    FILE *pidFile = fopen("server.pid", "w");
+    pid_t pid = getpid();
+    fprintf(pidFile, "%d", (int)pid);
+    fclose(pidFile);
 
+    signal(SIGINT, signal_handler);
+    signal(SIGQUIT, signal_handler);
 
-// message struct 
-struct mesg_buffer { 
-	long mesg_type; 
-	int mesg[100]; 
-} message; 
+    while(1);
 
-//Print Haiku function
-
-
-
-
-//Main function
-int main(){
-
-
-	//declare variables
-
-
- 	int msgid; 
-    key_t key;
-
-
- 	key = ftok("/etc/passwd", 'A'); 
-    if(key<0){
-        printf("ftok error \n");
-        exit(1);
-    }
-    
-	msgid = msgget(key, 0666 | IPC_CREAT); 
-    for(int i=0;i<100;i++){
-   
-        msgrcv(msgid, &message, sizeof(message), 1, 0); 
-        if(message.mesg[i]==2){
-                printf("%d) japaneese\n",i);
-            }else if(message.mesg[i]==3){
-                    printf("%d) western\n",i);
-                    }else{
-                        printf("%d) no %d\n",i,message.mesg[i]);
-                    }
-
-        
-    }
-    //remove the message queue
-	msgctl(msgid, IPC_RMID, NULL); 
-
-    //remove the semaphores
-
-
-
-
-return 0;
+    return 0;
 }
